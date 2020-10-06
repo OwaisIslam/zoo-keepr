d97cbc21619a6f0affce55e073e268c7df728667
Owais Islam <owaisislam@gmail.com>
2020-10-05 21:28:46 -0500

    fixed POST

--- server.js
+++ server.js
@@ -88,17 +88,12 @@ function validateAnimal(animal) {
 
 
 
-app.post('/api/animals', (req, res) => {
-    // set id based on what the next index of the array will be
-    req.body.id = animals.length.toString();
-
-    // if any data in req.body is incorrect, send 400 error back
-    if (!validateAnimal(req.body)) {
-        res.status(400).send('The animal is not properly formatted.');
-    } else {
-        const animal = createNewAnimal(req.body, animals);
-        res.json(animal);
+app.get('/api/animals', (req, res) => {
+    let results = animals;
+    if (req.query) {
+        results = filterByQuery(req.query, results);
     }
+    res.json(results);
 });
 
 app.get('/api/animals/:id', (req, res) => {
